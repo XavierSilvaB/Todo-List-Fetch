f8bdebe4e1975f590740dea6e79f1fb143027b5a
Jechf <jesuse.chf@gmail.com>
2022-08-03 12:27:49 +0000

    terminado

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import "../../styles/todo.css"
 
 export const Todo = () => {
@@ -6,7 +6,9 @@ export const Todo = () => {
     const [tasklist , setTasklist] = useState([]);
     const [task , setTask] = useState("");
 
-    const BURL = "https://assets.breatheco.de/apis/fake/todos/user/j3suschf";
+    useEffect(()=>{getApilist()},[]);
+
+    const BURL = "https://assets.breatheco.de/apis/fake/todos/user/jechf";
     
     const INI_POST = {
         method: "POST",
@@ -22,7 +24,26 @@ export const Todo = () => {
           "Content-Type": "application/json"
         }
       }
-        
+      
+      //Carga la lista guardada previamente o crea el usuario si no existe
+    const getApilist = async () =>{
+
+        let resp = await fetch(BURL);
+        if(resp.ok){
+            let APIList = await resp.json();
+            setTasklist(APIList);
+            setTask("");
+            console.log("Lista guardada cargada con exito")
+        }else{
+            let createuser = await fetch(BURL , INI_POST)
+            if(createuser.ok){
+                console.log("usuario iniciado con exito");
+            }
+        }
+        return console.log("iniciado")
+    }
+
+        //Mensaje para mostrar numero de tareas pendiente      
     const mensaje2 = () =>{
         if(tasklist.length == 0){
             return "No hay tareas pendientes"
@@ -31,6 +52,7 @@ export const Todo = () => {
             return `${tasklist.length} tareas pendientes`
         }
     }
+        //Mensaje para mostrar en Boton Eliminar 
     const mensaje = () =>{
         if(tasklist.length == 0){
             return "No hay tareas para mostrar"
@@ -39,8 +61,10 @@ export const Todo = () => {
             return "Eliminar todas las Tareas"
         }
     }
+        //Manejar cada tarea individual
     const handlerTask = (event) => { setTask(event.target.value)}
    
+        //agregar tareas a la lista y cargarlas al usuario en API
     const handlerKeyPress = async (event) => {
         
         if (event.key == 'Enter' && task != "") {
@@ -69,13 +93,10 @@ export const Todo = () => {
                 setTask("");
             } else {
                 alert("intenta de nuevo, tienes un error");
-            }
-            
-            
+            }           
         }
-
     }
-    
+        //Borrar una tarea a la vez y eliminar todo si es la ultima e inicializar el usuario
     const handlerButtomDelete = async (indexid, actlist) => {
         console.log(actlist);
         const filterlist = actlist.filter((todo, index)=> index != indexid);
@@ -109,7 +130,7 @@ export const Todo = () => {
 		}
         }
 
-
+        //borrar todas las tareas e inicializar el usuario
     const handlerDelete = async () =>
         {
             setTasklist(tasklist.length = []);
@@ -129,8 +150,11 @@ export const Todo = () => {
         
 return (
    
-    <div className='row mt-5'>
-        <h1 id="ti">To do list</h1>
+    // pedir a user que ingrese un nombre para crear
+    //deberia cargar la lista al iniciar o crear el usuario si no existe
+    
+    <div className='row mt-5'>  
+        <h1 id="ti">To-do list</h1>
         <div className='col-4'>
         <div className='card-text border-dark mt-3'>
                     <div className="form-floating mb-3">
