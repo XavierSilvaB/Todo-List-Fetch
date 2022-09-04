0e3253c8b4d7f0d9043cfd7c6f3322d95eb153a7
XavierSilvaB <xavieraugustosilvabaute@gmail.com>
2022-09-04 23:08:23 +0000

    actualizacion componente home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,7 +6,8 @@ const Home = () => {
 	return (
 		<div className="container mx-0 text-center" id="div1">
 			<Todo/>
-			<p className="flex justify-content-start text-light"> Made By: <strong>Xavier Silva <i class="fa-solid fa-code"></i></strong></p>
+			<p className="flex justify-content-start text-light mb-1"> Made By : <strong>Xavier Silva</strong></p>
+			<p className="flex justify-content-start text-light mt-0"> impossible is nothing </p>
 		</div>
 	);
 };
